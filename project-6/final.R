7962b218b934f7a8dac0688a28804566877c5821
rango <sidya.galakho@umontreal.ca>
2025-04-17 01:23:49 -0400

    final project

--- project-6/final.R
+++ project-6/final.R
@@ -41,3 +41,44 @@ mean.chimio <- with(data.with.ps[data.with.ps$traitement == 0, ],
 
 causal_effect <- mean.radio - mean.chimio
 causal_effect
+
+set.seed(123)
+n_boot <- 1000
+boot_estimates <- numeric(n_boot)
+
+for (i in 1:n_boot) {
+  # Resample data with replacement
+  boot_sample <- data[sample(nrow(data), replace = TRUE), ]
+  
+  # Re-estimate propensity scores in bootstrap sample
+  ps_model <- glm(traitement ~ age + sexe + severite, 
+                  family = "binomial", 
+                  data = boot_sample)
+  
+  boot_sample$pscore <- predict(ps_model, type = "response")
+  
+  # Recompute IPTW weights
+  boot_sample$weight <- ifelse(boot_sample$traitement == 1,
+                               1 / boot_sample$pscore,
+                               1 / (1 - boot_sample$pscore))
+  
+  # Compute weighted means
+  mean_treated <- with(boot_sample[boot_sample$traitement == 1, ],
+                       weighted.mean(taille, weight))
+  mean_untreated <- with(boot_sample[boot_sample$traitement == 0, ],
+                         weighted.mean(taille, weight))
+  
+  # Store causal effect estimate
+  boot_estimates[i] <- mean_treated - mean_untreated
+}
+
+# Compute 95% confidence interval
+ci_lower <- quantile(boot_estimates, 0.025)
+ci_upper <- quantile(boot_estimates, 0.975)
+causal_effect <- mean(boot_estimates)
+
+# Output
+list(
+  causal_effect = causal_effect,
+  CI_95 = c(lower = ci_lower, upper = ci_upper)
+)
