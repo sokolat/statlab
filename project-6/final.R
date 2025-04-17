d9575239726596f59691817787adcfabdf01a9b4
rango <sidya.galakho@umontreal.ca>
2025-04-17 17:12:09 -0400

    final project

--- project-6/final.R
+++ project-6/final.R
@@ -39,46 +39,39 @@ mean.radio <- with(data.with.ps[data.with.ps$traitement == 1, ],
 mean.chimio <- with(data.with.ps[data.with.ps$traitement == 0, ],
                        weighted.mean(taille, weight))
 
-causal_effect <- mean.radio - mean.chimio
-causal_effect
+causal.effect <- mean.radio - mean.chimio
+causal.effect
 
 set.seed(123)
-n_boot <- 1000
-boot_estimates <- numeric(n_boot)
+n.boot <- 1000
+boot.estimates <- numeric(n.boot)
 
-for (i in 1:n_boot) {
-  # Resample data with replacement
-  boot_sample <- data[sample(nrow(data), replace = TRUE), ]
+for (i in 1:n.boot) {
+  boot.sample <- data[sample(nrow(data), replace = TRUE), ]
   
-  # Re-estimate propensity scores in bootstrap sample
-  ps_model <- glm(traitement ~ age + sexe + severite, 
+  ps.model <- glm(traitement ~ age + sexe + severite, 
                   family = "binomial", 
-                  data = boot_sample)
+                  data = boot.sample)
   
-  boot_sample$pscore <- predict(ps_model, type = "response")
+  boot.sample$pscore <- predict(ps.model, type = "response")
   
-  # Recompute IPTW weights
-  boot_sample$weight <- ifelse(boot_sample$traitement == 1,
-                               1 / boot_sample$pscore,
-                               1 / (1 - boot_sample$pscore))
+  boot.sample$weight <- ifelse(boot.sample$traitement == 1,
+                               1 / boot.sample$pscore,
+                               1 / (1 - boot.sample$pscore))
   
-  # Compute weighted means
-  mean_treated <- with(boot_sample[boot_sample$traitement == 1, ],
+  mean.radio <- with(boot.sample[boot.sample$traitement == 1, ],
                        weighted.mean(taille, weight))
-  mean_untreated <- with(boot_sample[boot_sample$traitement == 0, ],
+  mean.chimio <- with(boot.sample[boot.sample$traitement == 0, ],
                          weighted.mean(taille, weight))
   
-  # Store causal effect estimate
-  boot_estimates[i] <- mean_treated - mean_untreated
+  boot.estimates[i] <- mean.radio - mean.chimio
 }
 
-# Compute 95% confidence interval
-ci_lower <- quantile(boot_estimates, 0.025)
-ci_upper <- quantile(boot_estimates, 0.975)
-causal_effect <- mean(boot_estimates)
+ci.lower <- quantile(boot.estimates, 0.025)
+ci.upper <- quantile(boot.estimates, 0.975)
+causal.effect <- mean(boot.estimates)
 
-# Output
 list(
-  causal_effect = causal_effect,
-  CI_95 = c(lower = ci_lower, upper = ci_upper)
+  causal.effect = causal.effect,
+  CI.95 = c(lower = ci.lower, upper = ci.upper)
 )
